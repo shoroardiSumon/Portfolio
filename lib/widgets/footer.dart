647c7e80aa2b0b3edcb88419ec34a4a6af20ae7e
shoroardiSumon <sumonpust7@gmail.com>
2025-11-15 13:49:53 +0600

    Add version in footer

--- lib/widgets/footer.dart
+++ lib/widgets/footer.dart
@@ -1,13 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:portfolio/constants/content.dart';
 
 // Footer uses centralized content constants for texts and social links
 
-class Footer extends StatelessWidget {
+class Footer extends StatefulWidget {
   final Function(String) onLaunchURL;
 
   const Footer({super.key, required this.onLaunchURL});
+
+  @override
+  State<Footer> createState() => _FooterState();
+}
+
+class _FooterState extends State<Footer> {
+  String _version = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _loadVersion();
+  }
+
+  Future<void> _loadVersion() async {
+    try {
+      final info = await PackageInfo.fromPlatform();
+      setState(() {
+        _version = '${info.version}+${info.buildNumber}';
+      });
+    } catch (_) {
+      // ignore and leave version empty
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,6 +54,10 @@ class Footer extends StatelessWidget {
               style: Theme.of(context).textTheme.bodyMedium,
             ),
           ),
+          if (_version.isNotEmpty) ...[
+            const SizedBox(height: 6),
+            Text('Version: $_version', style: Theme.of(context).textTheme.bodySmall),
+          ],
           const SizedBox(height: 10),
           Semantics(
             label: 'Social Links',
@@ -36,17 +65,17 @@ class Footer extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 IconButton(
-                  onPressed: () => onLaunchURL(socialLinks['github'] ?? ''),
+                  onPressed: () => widget.onLaunchURL(socialLinks['github'] ?? ''),
                   icon: FaIcon(FontAwesomeIcons.github, size: 20),
                   tooltip: githubTooltip,
                 ),
                 IconButton(
-                  onPressed: () => onLaunchURL(socialLinks['linkedin'] ?? ''),
+                  onPressed: () => widget.onLaunchURL(socialLinks['linkedin'] ?? ''),
                   icon: FaIcon(FontAwesomeIcons.linkedin, size: 20),
                   tooltip: linkedinTooltip,
                 ),
                 IconButton(
-                  onPressed: () => onLaunchURL(socialLinks['email'] ?? ''),
+                  onPressed: () => widget.onLaunchURL(socialLinks['email'] ?? ''),
                   icon: const Icon(Icons.email),
                   tooltip: sendEmailTooltip,
                 ),
